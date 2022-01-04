5991fd79cd9f4dc05d8a51ba2736ae905ab4aa63
morgan <morgantappret@hotmail.fr>
2022-01-04 11:55:12 +0100

    ajout suppression panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,19 +1,19 @@
-let panier = JSON.parse(localStorage.getItem("panier"));
-panier.forEach(function(article){
-  let showCart = document.getElementById("cart__items")
-  showCart.innerHTML += `<article class="cart__item" data-id="{product-ID}">
+getBasket().forEach(function (article) {
+  let showCart = document.getElementById("cart__items");
+  showCart.innerHTML += `<article class="cart__item" data-id="${article.id}">
   <div class="cart__item__img">
-    <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+    <img src="${article.image}" alt="${article.txt}">
   </div>
   <div class="cart__item__content">
     <div class="cart__item__content__titlePrice">
-      <h2>Nom du produit</h2>
+      <h2>${article.name}</h2>
       <p>${article.price},00 €</p>
+      <p>${article.color}</p>
     </div>
     <div class="cart__item__content__settings">
       <div class="cart__item__content__settings__quantity">
         <p>Qté : </p>
-        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+        <input type="number" class="" name="${article.name}" min="1" max="100" value="${article.quantity}">
       </div>
       <div class="cart__item__content__settings__delete">
         <p class="deleteItem">Supprimer</p>
@@ -21,5 +21,87 @@ panier.forEach(function(article){
     </div>
   </div>
 </article>`;
-  })
-  
+});
+calcBasket();
+
+totalQuantity = document.getElementById("totalQuantity");
+totalPrice = document.getElementById("totalPrice");
+
+//enregistrement des données sur le localstorage
+function saveBasket(panier) {
+  localStorage.setItem("panier", JSON.stringify(panier));
+  calcBasket();
+}
+//récupération du local storage
+function getBasket() {
+  let panier = localStorage.getItem("panier");
+  if (panier == null) {
+    return [];
+  } else {
+    try { // permet d'éviter une cassure dans l'exécution du code
+      return JSON.parse(panier);
+    } catch (e) {
+      return [];
+    }
+  }
+}
+
+function changeQuantity(product, input) {
+  let panier = getBasket();
+  let foundProduct = panier.find((p) => p.id == product.dataset.id); //recherche si l'id du produit éxiste déjà
+  if (foundProduct != undefined) {
+    const quantity = parseInt(input.value, 10);
+    foundProduct.quantity = quantity; // foundProduct.quantity = foundProduct.quantity + quantity;
+    if (foundProduct.quantity <= 0) {
+      removeFromBasket(product);
+    } else {
+      saveBasket(panier);
+    }
+  }
+}
+
+function calcBasket() {
+  let panier = getBasket();
+  let count = 0;
+  let somme = 0;
+  panier.forEach((item) => {
+    count += item.quantity;
+    somme += item.price * item.quantity;
+  });
+  totalQuantity.textContent = count;
+  totalPrice.textContent = somme;
+}
+
+//supprimer un produit du panier
+function removeFromBasket(product) {
+  let panier = getBasket();
+  panier = panier.filter((i) => i.id != product.dataset.id); //! = retire seulement l'id demandé
+  saveBasket(panier);
+  product.remove();
+}
+//   let btnSupp = document.querySelector(".deleteItem");
+// btnSupp.addEventListener("click", removeFromBasket);
+// btnSupp.addEventListener("click", () => {
+//   removeFromBasket('truc');
+// });
+// btnSupp.addEventListener("click", removeFromBasket.bind(null, 'truc'));
+//  //Sélection du bouton supprimer
+//   let childSupp = document.getElementsByClassName("cart__item__content__settings__delete");
+//déléguation d'évènement
+const cartItems = document.getElementById("cart__items");
+if (cartItems) {
+  cartItems.addEventListener("click", function (event) {
+    const btn = event.target.closest(".deleteItem");
+    if (!btn) return;
+    const item = btn.closest(".cart__item");
+    if (item) removeFromBasket(item);
+  });
+  cartItems.addEventListener("change", function (event) {
+    const input = event.target.closest("input[type=number]");
+    if (!input) return;
+    const item = input.closest(".cart__item");
+    if (item) changeQuantity(item, input);
+  });
+}
+
+
--- front/js/prod.js
+++ front/js/prod.js
@@ -1,85 +1,82 @@
-
-if(!localStorage.getItem("panier")){
+if (!localStorage.getItem("panier")) {
   console.log("création du panier");
   let init = [];
-  localStorage.setItem("panier",(JSON.stringify(init)))
-  
-}else{
+  localStorage.setItem("panier", JSON.stringify(init));
+} else {
   console.log("panier existant");
 }
 
-  
-
-  let params = (new URL(document.location)).searchParams;
-  let id = params.get('id');
-  // console.log(id);
+let params = new URL(document.location).searchParams;
+let id = params.get("id");
+// console.log(id);
 
 let panier = JSON.parse(localStorage.getItem("panier"));
 console.log(panier);
-    
-  
-  fetch("http://localhost:3000/api/products/"+id)
-  .then(res => res.json())
-  .then(data => {
+
+fetch("http://localhost:3000/api/products/" + id)
+  .then((res) => res.json())
+  .then((data) => {
     let title = document.getElementById("title"),
       image = document.querySelector("article > .item__img"),
       newImg = document.createElement("img"),
       price = document.getElementById("price"),
       description = document.getElementById("description"),
       colors = document.getElementById("colors");
-      newImg.setAttribute("src",`${data.imageUrl}`);
-      newImg.setAttribute("alt",`${data.altTxt}`);
-      image.appendChild(newImg);
-      title.innerHTML = `${data.name}`;
-      price.innerHTML = `${data.price}`;
-      description.innerHTML = `${data.description}`;
-      data.colors.forEach(color =>{
-         const newColor = document.createElement("option");
-         newColor.setAttribute("value",`${color}`);
-         newColor.textContent = `${color}`;
-         colors.appendChild(newColor)
-      })
-      
-      
-          let selectColor = document.getElementById("colors");
-          let quantity = document.getElementById("quantity");
-      let btn = document.getElementById("addToCart")
-      btn.addEventListener("click",function(){
-        let qty=parseInt(quantity.value, 10);
-        let newData = {
-          id: data._id,
-          name: data.name,
-          price: data.price,
-          description: data.description,
-          image: data.imageUrl,
-          txt: data.altTxt,
-          quantity: qty,
-          color: selectColor.value,
-           };
-          //  let tryId = panier.find(element => element.id == newData.id && element.color == newData.color);
-           let tryId = panier.find(function(element) {
-              return element.id == newData.id && element.color == newData.color
-            });
-        console.log(tryId);
-        
-        
-        if(tryId){
-          tryId.quantity += qty;
-          console.log("ok");
-          console.log(panier);
-          localStorage.setItem("panier",(JSON.stringify(panier)));
-        }else{
-          panier.push(newData);
-          localStorage.setItem("panier",(JSON.stringify(panier)));
-          console.log("pas ok");
-        };
-        panier.forEach(select =>
-          items.appendChild())
-  })  
-})
-  
-      
-  .catch(err => console.error(err));
+    newImg.setAttribute("src", `${data.imageUrl}`);
+    newImg.setAttribute("alt", `${data.altTxt}`);
+    image.appendChild(newImg);
+    title.innerHTML = `${data.name}`;
+    price.innerHTML = `${data.price}`;
+    description.innerHTML = `${data.description}`;
+    data.colors.forEach((color) => {
+      const newColor = document.createElement("option");
+      newColor.setAttribute("value", `${color}`);
+      newColor.textContent = `${color}`;
+      colors.appendChild(newColor);
+    });
 
-  /*Partie bouton et panier*/
-  
+    let selectColor = document.getElementById("colors");
+    let quantity = document.getElementById("quantity");
+    let btn = document.getElementById("addToCart");
+    btn.addEventListener("click", function () {
+      let qty = parseInt(quantity.value, 10);
+      if(qty < 1) {
+        alert("La quantité n'est pas correcte");
+        return;
+      }
+      if(!selectColor.value.length) {
+        alert("Veuillez choisir une couleur");
+        return;
+      }
+      let newData = {
+        id: data._id,
+        name: data.name,
+        price: data.price,
+        description: data.description,
+        image: data.imageUrl,
+        txt: data.altTxt,
+        quantity: qty,
+        color: selectColor.value,
+      };
+      //  let tryId = panier.find(element => element.id == newData.id && element.color == newData.color);
+      let tryId = panier.find(function (element) {
+        return element.id == newData.id && element.color == newData.color;
+      });
+
+      if (tryId) {
+        tryId.quantity += qty;
+        console.log("append");
+      } else {
+        panier.push(newData);
+        console.log("new");
+      }
+      localStorage.setItem("panier", JSON.stringify(panier));
+      console.log(panier);
+      // panier.forEach(select =>
+      //   items.appendChild())
+    });
+  })
+
+  .catch((err) => console.error(err));
+
+/*Partie bouton et panier*/
